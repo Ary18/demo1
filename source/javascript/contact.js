04fc428b9659c98bea00c046fa29938248285e37
Arianna Garassino <g.arianna26@gmail.com>
2018-02-23 10:22:09 +0100

    fatta funzionare funzione post al server

--- source/javascript/contact.js
+++ source/javascript/contact.js
@@ -4,10 +4,25 @@ $('#Send').click(function(){
     var name = $('#name').val();
     var surname = $('#surname').val();
     var phone = $('#phone').val();
-    var name = $('#name').val();
+    var nascita = $('#data').val();
+    var citta = $('#città').val();
+    var email = $('#email').val();
+    var textarea = $('#textarea').text();
+
+    var persona = {
+        "name" : {
+            "first": name,
+            "last": surname,
+        },
+        "phone": phone,
+        "Location":{
+            "city": citta,
+        },
+        "email": email,
+    };
     
     $.post("http://localhost:3000/results", persona)
-    .done(function(data){
-        alert("Data loaded" + data);
+    .done(function(){
+        alert("Data loaded");
     });
 });
