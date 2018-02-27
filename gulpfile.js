e716a51693cbcd1aab2bee7296628a945170fa2b
Arianna Garassino <g.arianna26@gmail.com>
2018-02-27 17:28:13 +0100

    aggiunto sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 // jshint ignore:start
+//jshint esversion:6
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
+const sass = require('gulp-sass');
 
 gulp.task('jshint',function(){
     return gulp.src('source/javascript/*.js').pipe(jshint())
@@ -13,4 +15,14 @@ gulp.task('copiaHtml',function(){
     gulp.src('source/html/*.html').pipe(gulp.dest('dist/html'));
     gulp.src('source/css/*.css').pipe(gulp.dest('dist/css'));
     gulp.src('source/javascript/*.js').pipe(gulp.dest('dist/javascript'));
+});
+
+gulp.task('sass', function (){
+    return gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function (){
+    gulp.watch('./sass/*.scss', ['sass']);
 });
